329340a1d2260e4272dc0e689657197dff4f1a11
khamdullaevuz <elbek.khamdullaev@gmail.com>
2023-07-03 14:06:00 +0500

    initial commit

---
+++ resources/js/Pages/Movies/Index.vue
@@ -0,0 +1,46 @@
+<template>
+    <table>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+        </tr>
+        <tr v-for="movie in movies" :key="movie.id">
+            <td>{{ movie.id }}</td>
+            <td>{{ movie.title }}</td>
+            <td>{{ movie.description }}</td>
+        </tr>
+    </table>
+</template>
+
+<script>
+export default {
+    props: {
+        movies: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+table {
+    border-collapse: collapse;
+    width: 100%;
+}
+
+th, td {
+    text-align: left;
+    padding: 8px;
+}
+
+tr:nth-child(even) {
+    background-color: #f2f2f2;
+}
+
+th {
+    background-color: #4CAF50;
+    color: white;
+}
+</style>
